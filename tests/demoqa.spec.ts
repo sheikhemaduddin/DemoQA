a45a2e7a59d1666c9bc18a9ea4203d2b44ca6ae4
Emad <sheikhemad94@gmail.com>
2022-05-04 04:29:58 +0500

    Commit

--- tests/demoqa.spec.ts
+++ tests/demoqa.spec.ts
@@ -22,6 +22,6 @@ test('FrameTest', async({page, context}) => {
     await page.mouse.down();
     await page.locator('body > div:nth-child(6) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)').click();
     await page.locator('div[class="element-list collapse show"] li[id="item-2"] span[class="text"]').click(); 
-    const HeadingText = await page.frameLocator('#frame1Wrapper').locator("#sampleHeading");
+    const HeadingText = await page.frameLocator('#frame1').locator("#sampleHeading");
     await expect(HeadingText).toContainText('This is a sample page');
   });
